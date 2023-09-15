ace83b45e816b9fe708e6c9356dd49d432056ff0
dimebruce <adriangarcia@qq.com>
2023-09-15 16:02:26 -0600

    Página de Primer Semestre

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Cultura Digital l</h1>
+      {/* Para ocultar informacion del iFrame se usa este code
+      rel=0&amp;showinfo=0 */}
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/videoseries?rel=0&amp;showinfo=0si=QbTsEe-RzLX5ZSwE&amp;list=PLhHbKDQSv0OmRVhRmjlgGh8pLrsL4FlHH"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+      <p className="read-the-docs">
+      </p>
+    </>
+  );
+}
+
+export default App;
